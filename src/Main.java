5af494cda1da9314da7153d437c255325f518dc6
TERrRISss <eogfpro@gmail.com>
2023-09-13 10:52:19 +0300

    Revert "Update Main.java"
    This reverts commit cdd8776fb16aebabf25fbad9e6653f3cdee29ab1.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Gun m4a1 = new Gun;
+        /*Gun m4a1 = new Gun;
             m4a1.getAmmo();
             m4a1.getType();
             m4a1.getEndurance();
-            m4a1.getHit(); 
-            m4a1.getColor();
+            m4a1.getHit(); */
+
 
 
     }
